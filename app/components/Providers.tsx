8681670c7e0af69272e4d097a0c3eaaaadac0de2
Muhammad-javed440 <meherjaved440@gmail.com>
2024-10-31 03:54:59 +0500

    enter variables

--- app/components/Providers.tsx
+++ app/components/Providers.tsx
@@ -6,12 +6,14 @@ import { CartProvider as USCProvider } from "use-shopping-cart";
 
 
 export default function CartProvider({ children }: { children: ReactNode }) {
-    
+  const stripeSecretKey = process.env.STRIPE_SECRET_KEY;
+
   return (
     <USCProvider
       mode="payment"
       cartMode="client-only"
       stripe={process.env.NEXT_PUBLIC_STRIPE_KEY as string}
+
       successUrl="https://localhost:3000/stripe/success"
       cancelUrl="https://localhost:3000/stripe/error"
       currency="USD"
